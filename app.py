98ae5574b3594a9006fdb8f6c93a4ba2736ca6b7
AlfaLachidua <87879826+AlfaLachidua@users.noreply.github.com>
2022-10-20 08:53:41 -0600

    se insertó imagen

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 import streamlit as st 
 st.title("Mi aplicación") #título
 st.sidebar.header("Esta aplicación es solo un demo")
-#st.sidebar.image("")
+st.sidebar.image("https://cdn.dribbble.com/userupload/3250764/file/original-8901511465fba8c4f7a4329ae6a9a565.png?compress=1&resize=1024x768")
 boton = st.button("globos")
 if boton:
   st.balloons()
