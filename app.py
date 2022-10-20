b5682bb8e0d9f3aaedaf01f8b108cd3780236fd3
AlfaLachidua <87879826+AlfaLachidua@users.noreply.github.com>
2022-10-20 09:48:03 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-#importar las librerías
+#########app.py del modelo ML_Streamlit_Colab
+#el llamado a la librería streamlit con el alias st
 import streamlit as st 
-st.title("Mi aplicación") #título
-st.sidebar.header("Esta aplicación es solo un demo")
-st.sidebar.image("https://cdn.dribbble.com/userupload/3250764/file/original-8901511465fba8c4f7a4329ae6a9a565.png?compress=1&resize=1024x768")
-boton = st.button("globos")
-if boton:
-  st.balloons()
-  
-  
+#hacemos el llamado a la función
+from img_classification import teachable_machine_classification 
+import keras
+from PIL import Image, ImageOps
+import numpy as np
+#le ponemos un título a nuestra aplicación
+st.title('Clasificando Vida Marina')
+#le voy a decir que tipos de archivos está recibiendo:
+uploaded_file = st.file_uploader("Carga una imagen ...", type=["jpg","jpeg"])
+#si el contenido no está vacío, se va a leer con el comando image
+if uploaded_file is not None:
+  image = Image.open(uploaded_file)
+  #le voy a decir que me despliegue en la página la misma imagen
+  st.sidebar.image(image)
+  #aquí estoy llamando la función teachable machine, va a leer la imagen e importar el modelo keras
+  #la salida de esta función va a ser una etiqueta 0 si es un perro, 1 si es un gato
+  label = teachable_machine_classification(image, 'keras_model.h5')
+  if label == 0:
+    st.write('Es un delfín') 
+  elif label == 1:
+    st.write('Es un pinguino') 
+
+  else:
+    st.write('Es un pulpo')
